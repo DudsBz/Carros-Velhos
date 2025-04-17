f6b2a22b2424410271460e5d344d1299a8e50740
DudsBZ <20dubu06@gmail.com>
2025-04-16 23:53:52 -0300

    Banco interno

---
+++ src/database.py
@@ -0,0 +1,48 @@
+cars = [
+    {
+        "id": 1,
+        "marca": "Ford",
+        "modelo": "Crestline",
+        "ano": 1952,
+        "preco": 200000.00,
+        "cor": "Vermelho",
+        "foto": "crestline.jpg"
+    },
+    {
+        "id": 2,
+        "marca": "Chevrolet",
+        "modelo": "3100",
+        "ano": 1947,
+        "preco": 100000.00,
+        "cor": "Azul-Bebe",
+        "foto": "3100.jpg"
+    },
+    {
+        "id": 3,
+        "marca": "Hudson Motor Car Company",
+        "modelo": "Hudson Hornet",
+        "ano": 1951,
+        "preco": 150000.00,
+        "cor": "Azul-Bebe",
+        "foto": "hudsonhornet.jpg"
+    },
+    {
+        "id": 4,
+        "marca": "Volkswagen",
+        "modelo": "Karmann Ghia",
+        "ano": 1968,
+        "preco": 200000.00,
+        "cor": "Vinho",
+        "foto": "karmannGhia.jpg"
+    },
+    {
+        "id": 5,
+        "marca": "Dodge",
+        "modelo": "Dodge Super Bee",
+        "ano": 1970,
+        "preco": 400000.00,
+        "cor": "Vermelho",
+        "foto": "dodge.jpg"
+    },
+
+]
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import flet as ft
+from database import cars
+
+def main(page: ft.Page):
+    
+
+    page.add(
+        
+    )
+    
+
+
+ft.app(main)
