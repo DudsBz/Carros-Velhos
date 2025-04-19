88a6c0c7ad5eb280cc18d1116eb66a444039f32d
DudsBZ <20dubu06@gmail.com>
2025-04-18 23:12:28 -0300

    Arrumando a parte de scroll e evento de 'delete'

--- src/database.py
+++ src/database.py
@@ -6,6 +6,7 @@ cars = [
         "ano": 1952,
         "preco": 200000.00,
         "cor": "Vermelho",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "foto": "crestline.jpg"
     },
     {
@@ -15,6 +16,7 @@ cars = [
         "ano": 1947,
         "preco": 100000.00,
         "cor": "Azul-Bebe",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "foto": "3100.jpg"
     },
     {
@@ -24,6 +26,7 @@ cars = [
         "ano": 1951,
         "preco": 150000.00,
         "cor": "Azul-Bebe",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "foto": "hudsonhornet.jpg"
     },
     {
@@ -33,6 +36,7 @@ cars = [
         "ano": 1968,
         "preco": 200000.00,
         "cor": "Vinho",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "foto": "karmannGhia.jpg"
     },
     {
@@ -42,7 +46,107 @@ cars = [
         "ano": 1970,
         "preco": 400000.00,
         "cor": "Vermelho",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "foto": "dodge.jpg"
     },
-
+    {
+        "id": 6,
+        "marca": "Rolls-Royce",
+        "modelo": "Silver Ghost",
+        "ano": 1913,
+        "preco": 1500000.00,
+        "cor": "Prata",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "silverghost.jpg"
+    },
+    {
+        "id": 7,
+        "marca": "Mercedes-Benz",
+        "modelo": "300 SL Gullwing",
+        "ano": 1955,
+        "preco": 1200000.00,
+        "cor": "Branco",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "300sl.jpg"
+    },
+    {
+        "id": 8,
+        "marca": "Jaguar",
+        "modelo": "E-Type",
+        "ano": 1961,
+        "preco": 350000.00,
+        "cor": "Vermelho",  
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "etype.jpg"
+    },
+    {
+        "id": 9,
+        "marca": "Tesla",
+        "modelo": "Model S",
+        "ano": 2023,
+        "preco": 500000.00,
+        "cor": "Preto",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "teslas.jpg"
+    },
+    {
+        "id": 10,
+        "marca": "Porsche",
+        "modelo": "911 Carrera RS 2.7",
+        "ano": 1973,
+        "preco": 800000.00,
+        "cor": "Branco com detalhes em azul",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "911rs.png"
+    },
+    {
+        "id": 11,
+        "marca": "Ford",
+        "modelo": "Mustang Mach-E",
+        "ano": 2024,
+        "preco": 300000.00,
+        "cor": "Azul",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "mach-e.jpg"
+    },
+    {
+        "id": 12,
+        "marca": "Cadillac",
+        "modelo": "Eldorado Biarritz",
+        "ano": 1959,
+        "preco": 600000.00,
+        "cor": "Rosa",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "eldorado.jpg"
+    },
+    {
+        "id": 13,
+        "marca": "DeLorean",
+        "modelo": "DMC-12",
+        "ano": 1981,
+        "preco": 450000.00,
+        "cor": "Inox",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "dmc12.jpg"
+    },
+    {
+        "id": 14,
+        "marca": "BMW",
+        "modelo": "iX",
+        "ano": 2023,
+        "preco": 480000.00,
+        "cor": "Cinza",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "ix.jpg"
+    },
+    {
+        "id": 15,
+        "marca": "Chevrolet",
+        "modelo": "Bel Air",
+        "ano": 1957,
+        "preco": 250000.00,
+        "cor": "Turquesa",
+        "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "foto": "belair.jpg"
+    },
 ]
--- src/main.py
+++ src/main.py
@@ -6,15 +6,20 @@ def main(page: ft.Page):
     def check_item_clicked(e):
         e.control.checked = not e.control.checked
         page.update()
-
-    """def clicar(e):
+    def mostrar_descricao(e):
+        car = next((car for car in cars if car['id'] == e.control.parent.key), None)
         dlg = ft.AlertDialog(
-            title=ft.Text('Cliquei'),
+            title=ft.Text(car['descricao'], size=12),
             actions=[
-                ft.TextButton('Fechar', on_click=lambda e: page.close(dlg)),
+                ft.TextButton("Fechar", on_click=lambda e: page.close(dlg)),
             ],
         )
-        page.open(dlg)"""
+        page.open(dlg)
+    def deletar_carro(e):
+        e.control.parent.parent.visible = False
+        page.update()
+
+    
 
     page.title = "Carros"
     page.window.width = 400
@@ -29,7 +34,7 @@ def main(page: ft.Page):
         actions=[
             ft.IconButton(
                 icon=ft.icons.NOTIFICATIONS,
-                icon_color=ft.colors.PURPLE,
+                icon_color=ft.colors.ON_SURFACE_VARIANT,
             ),
             ft.PopupMenuButton(
                 items=[
@@ -56,7 +61,7 @@ def main(page: ft.Page):
     cars_list = ft.Column(
         scroll=ft.ScrollMode.ALWAYS,
         expand=True,
-        spacing=10,
+        
 
     )
     for car in cars:
@@ -69,7 +74,24 @@ def main(page: ft.Page):
                 height=100,
                 border_radius=ft.border_radius.all(5),
             ),
-            title=ft.Text(f"{car['marca']} - {car['modelo']}"),
+            title=ft.Text(f"{car['marca']} - {car['modelo']}", size=12),
+            subtitle=ft.Text(f"Ano: {car['ano']} - Cor: {car['cor']}", size=10),
+            trailing=ft.PopupMenuButton(
+                key=car['id'],
+                icon=ft.icons.MORE_VERT,
+                items=[
+                    ft.PopupMenuItem(
+                        text="Ver Detalhes",
+                        icon=ft.icons.INFO,
+                        on_click=mostrar_descricao,
+                    ),
+                    ft.PopupMenuItem(
+                        text="Remover",
+                        icon=ft.icons.DELETE,
+                        on_click=deletar_carro,
+                    ),
+                ],
+            ),
         )
         cars_list.controls.append(car_component)
 
